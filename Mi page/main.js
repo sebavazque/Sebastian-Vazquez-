4e831bcaff673c5ffc2cfdeaf15c2ee81a697532
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-09-05 15:39:24 -0300

    Add files via upload

---
+++ Mi page/main.js
@@ -0,0 +1,9 @@
+let nombreApellido = prompt ('Ingrese su nombre y apellido');
+let nuestraEdad = prompt ('Ahora ingrese su edad');
+
+let edad = parseInt(nuestraEdad); 
+
+console.log(nombreApellido + " " + nuestraEdad);
+
+ 
+
