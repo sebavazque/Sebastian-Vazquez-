d0a9fedc2eec9dce8873617dd6d0e0b12e0bf6c2
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-09-05 15:38:41 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let nombreApellido = prompt ('Ingrese su nombre y apellido');
-let nuestraEdad = prompt ('Ahora ingrese su edad');
-
-let edad = parseInt(nuestraEdad); 
-
-console.log(nombreApellido + " " + nuestraEdad);
-
- 
-
