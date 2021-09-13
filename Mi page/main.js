d3db8fcdd65415d30371d220770c2d50dd569431
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-09-13 20:19:00 -0300

    Add files via upload

---
+++ Mi page/main.js
@@ -0,0 +1,5 @@
+let multi =parseInt (prompt("Ingrese un numero para mostrar su table"));
+for (let numero = 1; numero <=10; numero++){  
+    let resultado = multi * numero;   
+    console.log (multi + "x" + numero + "=" + resultado)
+}
