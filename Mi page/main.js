b188787123c05589820abbaddb37db9cd67388d3
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-09-16 18:55:41 -0300

    Delete main.js

--- Mi page/main.js
+++
@@ -1,5 +0,0 @@
-let multi =parseInt (prompt("Ingrese un numero para mostrar su table"));
-for (let numero = 1; numero <=10; numero++){  
-    let resultado = multi * numero;   
-    console.log (multi + "x" + numero + "=" + resultado)
-}
