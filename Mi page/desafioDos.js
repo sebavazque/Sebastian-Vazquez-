30e800c7d5374141ea87dc1e33315cd13f306749
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-10-05 22:57:23 -0300

    Add files via upload

---
+++ Mi page/desafioDos.js
@@ -0,0 +1,6 @@
+let  ingresarEdad = prompt("Ingrese su edad");
+if ( ingresarEdad >'17'){
+    alert("Bienvido a Hechito con Amor");
+}else{
+    alert("Para ingresar a este sitio tiene que ser mayor de edad");
+}
