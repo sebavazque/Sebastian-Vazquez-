7d064934b89b590fa9ba8553b999442d6485718c
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-10-11 22:53:28 -0300

    Add files via upload

---
+++ Mi page/desafioTres.js
@@ -0,0 +1,6 @@
+let  ingresarAño = prompt("Ingrese el año actual");
+let  ingresarEdad = prompt("Ingrese su año de nacimiento");
+let resultado = ("Su edad es :");
+let resultadoAño = (ingresarAño - ingresarEdad);
+
+alert(resultado + " " + resultadoAño);
