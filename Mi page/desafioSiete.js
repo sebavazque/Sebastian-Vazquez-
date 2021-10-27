0006444b56e4c865d6633340c5c48726904cb9a1
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-10-26 23:08:36 -0300

    Add files via upload

---
+++ Mi page/desafioSiete.js
@@ -0,0 +1,5 @@
+document.getElementById("miImagen").src = "image/fotos (3).jpeg";
+
+document.getElementById("miImagen2").src = "image/fotos (4).jpeg";
+
+document.getElementById("miImagen3").src = "image/almohada.jpeg";
