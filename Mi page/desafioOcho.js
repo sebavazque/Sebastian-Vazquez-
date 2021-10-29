40243a1685af4ab9002daa7c2ec7f4cb3f31aee8
sebavazque <88560731+sebavazque@users.noreply.github.com>
2021-10-29 17:41:06 -0300

    Add files via upload

---
+++ Mi page/desafioOcho.js
@@ -0,0 +1,38 @@
+document.getElementById("miImagen").src = "image/fotos (3).jpeg";
+
+document.getElementById("miImagen2").src = "image/fotos (4).jpeg";
+
+document.getElementById("miImagen3").src = "image/almohada.jpeg";
+
+
+document.getElementById("boton1").addEventListener("click", function(){
+    alert("Este producto cuesta $700");
+});
+
+document.getElementById("boton2").addEventListener("click", function(){
+    alert("Este producto cuesta $800");
+});
+
+document.getElementById("boton3").addEventListener("click", function(){
+    alert("Este producto cuesta $5000");
+});
+
+document.getElementById("boton4").addEventListener("click", function(){
+    alert("Este producto cuesta $2800");
+});
+
+document.getElementById("boton5").addEventListener("click", function(){
+    alert("Este producto cuesta $3800");
+});
+
+document.getElementById("boton6").addEventListener("click", function(){
+    alert("Este producto cuesta $4800");
+});
+
+document.getElementById("boton7").addEventListener("click", function(){
+    alert("Este producto cuesta $1800");
+});
+
+document.getElementById("boton8").addEventListener("click", function(){
+    alert("Este producto cuesta $2800");
+});
